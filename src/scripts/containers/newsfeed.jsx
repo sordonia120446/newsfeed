6934fc57db5e746de5ceac9a4879fbdf358656c4
Samuel Ordonia <samuel.ordonia@gmail.com>
2019-08-07 23:17:33 -0700

    wire up data input

--- src/scripts/containers/newsfeed.jsx
+++ src/scripts/containers/newsfeed.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import topHeadlines from "../data/top_headlines";
+
 
 export default class NewsFeed extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      data: {},
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ data: topHeadlines, loading: false });
+  }
+
   render() {
     return (
       <div>
+        {/* This will eventually be another component */}
         <h1>hi</h1>
       </div>
     )
