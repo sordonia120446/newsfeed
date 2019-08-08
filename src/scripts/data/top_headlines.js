576d6019120e38c80e8b45b82246ff1942d07ba7
Samuel Ordonia <samuel.ordonia@gmail.com>
2019-08-07 23:12:22 -0700

    get rid of useless escaping

--- src/scripts/data/top_headlines.js
+++ src/scripts/data/top_headlines.js
@@ -13,8 +13,8 @@ const topHeadlines = {
       "author": "BBC News",
       "title": "UN 'deeply concerned' over Kashmir restrictions",
       "description": "It says India's telecommunications ban and other moves will \"will exacerbate the human rights situation\".",
-      "url": "http:\/\/www.bbc.co.uk\/news\/world-asia-india-49273999",
-      "urlToImage": "https:\/\/ichef.bbci.co.uk\/news\/1024\/branded_news\/18487\/production\/_108236499_298af849-fd88-44d2-91a1-5c19c6846996.jpg",
+      "url": "http://www.bbc.co.uk/news/world-asia-india-49273999",
+      "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/18487/production/_108236499_298af849-fd88-44d2-91a1-5c19c6846996.jpg",
       "publishedAt": "2019-08-08T03:19:33Z",
       "content": "Image copyrightEPAImage caption\r\n The region remains in lockdown\r\nThe UN has said the latest restrictions imposed on Indian-administered Kashmir are deeply concerning and \"will exacerbate the human rights situation\".\r\nA UN spokesperson highlighted a telecommu\u2026 [+2199 chars]"
     },
@@ -26,8 +26,8 @@ const topHeadlines = {
       "author": "BBC News",
       "title": "Puerto Rico swears in new governor",
       "description": "Wanda V\u00e1zquez is the US territory's new governor after her predecessor is forced to stand down.",
-      "url": "http:\/\/www.bbc.co.uk\/news\/world-us-canada-49273241",
-      "urlToImage": "https:\/\/ichef.bbci.co.uk\/news\/1024\/branded_news\/2EB1\/production\/_108235911_055733275.jpg",
+      "url": "http://www.bbc.co.uk/news/world-us-canada-49273241",
+      "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/2EB1/production/_108235911_055733275.jpg",
       "publishedAt": "2019-08-08T00:11:26Z",
       "content": "Image copyrightEPAImage caption\r\n Wanda V\u00e1zquez, left, was sworn in with her husband and daughter by her side\r\nPuerto Rico's Justice Secretary Wanda V\u00e1zquez has been sworn in as the US territory's new governor, hours after her predecessor was forced to stand \u2026 [+1585 chars]"
     }
