9c72ad6af31ba903e0aac0ae68d323cf08d83239
Samuel Ordonia <samuel.ordonia@gmail.com>
2019-08-10 11:34:59 -0700

    touch up the hr things

--- src/scripts/components/headline.jsx
+++ src/scripts/components/headline.jsx
@@ -5,12 +5,12 @@ import '../../static/headline.css';
 const Headline = props => {
   return (
     <div className="headline-wrapper drop-shadow">
+      <hr />
       <h2>{props.title}</h2>
       <h4>{props.author}</h4>
       <img src={props.urlToImage} alt="" className="image" />
       <p>{props.description}</p>
       <a href={props.url} target="_blank" rel="noopener noreferrer">Read more</a>
-      <hr />
     </div>
   )
 }
