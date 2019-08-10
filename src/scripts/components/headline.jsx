56b4168c09f3560c15dbb10c9d5941acf9b012f8
Samuel Ordonia <samuel.ordonia@gmail.com>
2019-08-09 19:05:39 -0700

    add some style polishes

--- src/scripts/components/headline.jsx
+++ src/scripts/components/headline.jsx
@@ -4,12 +4,13 @@ import '../../static/headline.css';
 
 const Headline = props => {
   return (
-    <div className="headline-wrapper">
+    <div className="headline-wrapper drop-shadow">
       <h2>{props.title}</h2>
       <h4>{props.author}</h4>
       <img src={props.urlToImage} alt="" className="image" />
       <p>{props.description}</p>
       <a href={props.url} target="_blank" rel="noopener noreferrer">Read more</a>
+      <hr />
     </div>
   )
 }
