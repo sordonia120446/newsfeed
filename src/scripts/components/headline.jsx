1b320c12e8e03940b63a8da6be6803760504214d
Samuel Ordonia <samuel.ordonia@gmail.com>
2019-08-11 16:08:47 -0700

    get rid of the description

--- src/scripts/components/headline.jsx
+++ src/scripts/components/headline.jsx
@@ -8,7 +8,6 @@ const Headline = props => {
       <hr />
       <h2>{props.title}</h2>
       <h4>{props.author}</h4>
-      <p className="description">{props.description}</p>
       <a href={props.url} target="_blank" rel="noopener noreferrer">
         <img src={props.urlToImage} alt={props.description} className="image" />
       </a>
