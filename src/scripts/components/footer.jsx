d861a798a6cc0c2eecf89edbbc626dfe25c9331b
Samuel Ordonia <samuel.ordonia@gmail.com>
2019-08-11 21:41:12 -0700

    add footer with news api ref

---
+++ src/scripts/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../static/footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <p>Powered by <a href="https://newsapi.org/docs/get-started" target="_blank" rel="noopener noreferrer">News API</a></p>
+    </div>
+  )
+}
+
+export default Footer;
