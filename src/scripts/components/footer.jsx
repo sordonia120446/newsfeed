2a12b7272d97e940b1dc009af465abd9e9e37ae1
Samuel Ordonia <samuel.ordonia@gmail.com>
2019-08-15 19:02:53 -0700

    make the footer a little prettier

--- src/scripts/components/footer.jsx
+++ src/scripts/components/footer.jsx
@@ -5,7 +5,10 @@ import '../../static/footer.css';
 const Footer = () => {
   return (
     <div className="footer-wrapper">
+      <br />
+      <hr />
       <p>Powered by <a href="https://newsapi.org/docs/get-started" target="_blank" rel="noopener noreferrer">News API</a></p>
+      <br />
     </div>
   )
 }
